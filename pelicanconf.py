c11753dc7cd6b1f5fac8b5410ad273369fb823cf
tolerious <tolerious@qq.com>
2018-06-28 15:32:21 +0800

    update code ... [

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,23 +8,23 @@ SITEURL = ''
 SITEUNAME = u"冷眼看八卦,娱乐猛回头"
 
 PATH = 'content'
-MAIL='tolerious#qq.com'
-ABOUT_TEXT='This is tolerious.'
-ABOUT_IMAGE='images/picture.jpg'
-GOOGLEPLUS_USER='107031989496937042842'
-TWITTER_USER='tolerious'
-LINKEDIN_USER='tolerious'
-FACEBOOK_USER='tolerious'
-WEIBO_USER='51682963'
+MAIL = 'tolerious#qq.com'
+ABOUT_TEXT = 'This is tolerious.'
+ABOUT_IMAGE = 'images/picture.jpg'
+GOOGLEPLUS_USER = '107031989496937042842'
+TWITTER_USER = 'tolerious'
+LINKEDIN_USER = 'tolerious'
+FACEBOOK_USER = 'tolerious'
+WEIBO_USER = '51682963'
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
 DATE_FORMATS = {
-        'zh_CN': '%Y-%m-%d %H:%M',
+    'zh_CN': '%Y-%m-%d %H:%M',
 }
 DEFAULT_DATE_FORMAT = '%Y-%m-%d %H:%M'
-DEFAULT_DATE = 'fs' # use filesystem's mtime
+DEFAULT_DATE = (2013, 3, 2, 14, 1, 1)  # use filesystem's mtime
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -39,7 +39,7 @@ LINKS = (('同济大学出版社英文版', 'http://en.tongjipress.com.cn/'),
          ('同济创意设计', 'http://design.tongjix.cn/#/'),
          ('Github', 'https://github.com/tolerious'),
          #('Jinja2', 'http://jinja.pocoo.org/'),
-        )
+         )
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -47,6 +47,6 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 6
 STATIC_PATHS = ['images', 'pdfs']
-LOCALE = ('en_US','ja_JP')
+LOCALE = ('en_US', 'ja_JP')
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
--- publishconf.py
+++ publishconf.py
@@ -10,10 +10,11 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://tolerious.com'
+SITEURL = 'http://blog.xunzitech.cn'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
